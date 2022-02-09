1484cfe4758a865398728e570ad7f75fa688aa7f
Dominique Ducommun <hello@do.min.icu>
2022-02-09 11:28:31 +0100

    Update frontpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
             <svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
             </svg>
-            <p class="ml-9 text-lg leading-6 font-medium text-gray-900">Laravel8 // PHP8</p>
+            <p class="ml-9 text-lg leading-6 font-medium text-gray-900">Laravel {{ app()->version() }} // PHP8.1</p>
           </dt>
           <dd class="mt-2 ml-9 text-base text-gray-500">
             You can manage phone, email and chat conversations all from a single mailbox.
